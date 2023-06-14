bd837ecf184408c060424c94b360ee3aafde262f
olatzinfante <olatz.infante268@comunidadunir.net>
2023-06-14 13:21:34 +0200

    base.html

--- web/app.py
+++ web/app.py
@@ -15,12 +15,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("sample-page.html")
-
-@app.route("/presentacion")
-def presentacion():
     return render_template("index.html")
 
+@app.route("/dataset")
+def dataset():
+    return render_template("dataset.html")
+
+@app.route("/visualizacion")
+def visualizacion():
+    return render_template("visualizacion.html")
+
+@app.route("/eda")
+def eda():
+    return render_template("eda.html")
+
+@app.route("/machineLearning")
+def machineLearning():
+    return render_template("machineLearning.html")
+
+@app.route("/resultados")
+def resultados():
+    return render_template("resultados.html")
+
+@app.route("/sobre-nosotros")
+def sobre_nosotros():
+    return render_template("sobre-nosotros.html")
 
 
 
